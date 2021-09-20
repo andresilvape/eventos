ffb411b96bea406195df04684792929d46e90684
andresilva.pe@gmail.com <andresilva.pe@gmail.com>
2021-09-20 20:41:43 -0300

    update eventos

--- src/main/java/com/eventos/controller/EventoController.java
+++ src/main/java/com/eventos/controller/EventoController.java
@@ -2,6 +2,7 @@ package com.eventos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,7 +16,7 @@ public class EventoController {
 	@Autowired
 	EventoRepository eventorepository;
 	
-	@RequestMapping(value = "/eventos", method = RequestMethod.GET)
+	@GetMapping(value = "/eventos")
 	public String form() {
 		return "/formEvento";
 	}
