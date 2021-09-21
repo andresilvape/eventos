8514f682c2ee145d873bdbde308333cdddd70dff
andresilva.pe@gmail.com <andresilva.pe@gmail.com>
2021-09-21 00:29:28 -0300

    include images and css

--- src/main/java/com/eventos/controller/EventoController.java
+++ src/main/java/com/eventos/controller/EventoController.java
@@ -2,8 +2,10 @@ package com.eventos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eventos.domain.Evento;
@@ -14,9 +16,14 @@ public class EventoController {
 
 	@Autowired
 	EventoRepository eventorepository;
-	
+
+	/*
+	 * @RequestMapping(value = "/cadatro-de-eventos", method = RequestMethod.GET)
+	 * public String form() { return "/formeEvento"; }
+	 */
+
 	@RequestMapping(value = "/cadatro-de-eventos", method = RequestMethod.GET)
-	public String form() {
+	public String formeEvento() {
 		return "/formeEvento";
 	}
 
@@ -26,12 +33,12 @@ public class EventoController {
 
 		return "redirect:/cadatro-de-eventos";
 	}
-	
-	  @RequestMapping(value="/eventos-adicionados")
-	  public ModelAndView listaEventos() {
-	  ModelAndView mv = new ModelAndView("listarEvento"); 
-	  Iterable<Evento> eventos= eventorepository.findAll();
-	  mv.addObject("eventos", eventos);
-	  return mv;
-	  }
+
+	@RequestMapping(value = "/eventos-adicionados")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("listarEvento");
+		Iterable<Evento> eventos = eventorepository.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
 }
