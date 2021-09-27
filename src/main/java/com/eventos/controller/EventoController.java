3f51a7c5cdab840cd1dc4c656599951065a926da
andresilva.pe@gmail.com <andresilva.pe@gmail.com>
2021-09-27 14:11:54 -0300

    bd post

--- src/main/java/com/eventos/controller/EventoController.java
+++ src/main/java/com/eventos/controller/EventoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.eventos.domain.Evento;
 import com.eventos.repository.EventoRepository;
 
-@Controller
+@RestController 
 public class EventoController {
 
 	@Autowired
