becdb1260607b9afb747751c25912c909293afc0
andresilva.pe@gmail.com <andresilva.pe@gmail.com>
2021-09-27 14:05:28 -0300

    bd post

--- src/main/java/com/eventos/domain/Evento.java
+++ src/main/java/com/eventos/domain/Evento.java
@@ -12,7 +12,7 @@ public class Evento implements Serializable{
 	private static final long serialVersionUID= 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 
 	private String nome;
